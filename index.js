2eb401bbeebcee39d6f1f1e71a7669bbc3059b2a
Carlos May <mayfiend@hotmail.com>
2019-09-30 13:21:30 -0500

    Test act
    Test

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 const express = require('express');
 const app = express();
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-/*app.set('port',process.env.OPENSHIFT_NODEJS_PORT || 3000);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"); */
-
-app.get('/',(req,res)=>{
-    res.send('index page');
-})
-app.get('/random/:numeroInicial/:numeroFinal',(req,res)=>{
-    const min = parseInt(req.params.numeroInicial);
-    const max = parseInt(req.params.numeroFinal);
-    const result = Math.floor(Math.random() * (max - min) + min);
-    if(isNaN(min) || isNaN(max)){
-        res.status(404);
-        res.json({"error":'bad request'});
-        return;
-    }
-    res.json({"randomNumber":result});
-})
-
-// error handling
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500).send('Something bad happened!');
-  });
-  
-app.listen(port,ip,()=>{
-    console.log('server on port: ',port,' IP:',ip);
-})
-
-module.exports = app ;
+const http = require('http')
+const hostname = '127.0.0.1';
+const port = 3000;
+ 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World! NodeJS \n');
+});
+ 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
